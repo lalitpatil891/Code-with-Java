eec1812923a30ad4a3a6818406ad51f957a8c5c4
Lalit Patil <lp613408@gmail.com>
2025-07-11 23:18:26 +0530

    OddNumberSquareGenerator

---
+++ Practice_programs/src/oddNumberSquareGenerator/OddNumbersSquare.java
@@ -0,0 +1,23 @@
+package oddNumberSquareGenerator;
+
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class OddNumbersSquare {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+
+		List<Integer> oddSquares = Stream.iterate(1, i -> i + 2) // Generate odd numbers
+				.limit(n / 2 + 1) // Limit to odd numbers up to N
+				.map(i -> i * i) // Square each odd number
+				.collect(Collectors.toList());
+
+		System.out.println(oddSquares);
+	}
+
+}
