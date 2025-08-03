f2d97e5e6759e64f0120bfc68a7ec15391f72c6c
Lalit Patil <lp613408@gmail.com>
2025-08-03 12:53:54 +0530

    forEach(Consumer<T>) usage with different implementations

---
+++ CollectionsNotesAndPrograms/Notes/ways-fetch OR retrieve the Object/forEach(Consumer cons)/ForEachMethodInternalDemo2.java
@@ -0,0 +1,23 @@
+//Case 2
+package com.ravi.collection;
+
+import java.util.Vector;
+import java.util.function.Consumer;
+
+public class ForEachMethodInternalDemo1 {
+
+	public static void main(String[] args)
+	{
+		  Vector<String> listOfCity = new Vector<>();
+	       listOfCity.add("Hyderabad");
+	       listOfCity.add("Kolkata");
+	       listOfCity.add("Pune");
+	       listOfCity.add("Indore");
+	       listOfCity.add("Mumbai");
+
+	       //Lambda Expression
+	       Consumer<String> cons = city -> System.out.println(city.toUpperCase());
+
+	       listOfCity.forEach(cons);
+	}
+}
