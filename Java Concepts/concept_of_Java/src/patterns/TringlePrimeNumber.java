f0f1d4da9dc497e5b53f752a6791f6739de0f816
lalitpatil891 <lp613408@gmail.com>
2025-11-22 22:46:47 +0530

    Add programs on core java

---
+++ Java Concepts/concept_of_Java/src/patterns/TringlePrimeNumber.java
@@ -0,0 +1,13 @@
+package patterns;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class TringlePrimeNumber {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Sys");
+	}
+}
